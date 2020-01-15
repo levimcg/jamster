207daa2c38f2e5ce9d12f86270b55d6aad29e5f2
Levi <levimcg@gmail.com>
2020-01-14 20:36:40 -0500

    Refactored to use config files instead of wizard

--- index.js
+++ index.js
@@ -5,42 +5,38 @@ const fs = require('fs-extra');
 const matter = require('gray-matter');
 const pkg = require('./package.json');
 const program = require('commander');
-const { prompt } = require('inquirer');
-const questions = require('./lib/questions');
-
-function formatDate(date) {
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
-  return `${year}-${month}-${day}`;
-}
+const { cosmiconfigSync } = require('cosmiconfig');
 
 program.version(pkg.version);
 program
+  .option('-c, --config <configFile>')
   .arguments('<file>')
   .action(file => {
-    prompt(questions)
-      .then(answers => {
-        const currentWorkingDir = process.cwd();
-        const filePath = `${currentWorkingDir}/${file}`;
-        const frontMatterContents = matter.stringify('', {
-          title: answers.title,
-          description: answers.description,
-          date: formatDate(new Date()),
-          tags: answers.tags
-        });
+    let config;
+    const explorer = cosmiconfigSync('md-generate');
+    if (program.config) {
+      config = explorer.load(`./${program.config}`);
+    } else {
+      config = explorer.search();
+    }
+    
+    if (!config) {
+      config = require('./lib/defaults');
+    }
+    const currentWorkingDir = process.cwd();
+    const filePath = `${currentWorkingDir}/${file}`;
+    const frontMatterContents = matter.stringify('', config.config);
 
-        fs.outputFile(filePath, frontMatterContents, err => {
-          if (err) {
-            console.log(err);
-            process.exit(1);
-          } else {
-            console.log(
-              chalk.green.bold(`\n 🎉 A new file was create at: ${filePath} \n`)
-            );
-          }
-        });
-      })
+    fs.outputFile(filePath, frontMatterContents, err => {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      } else {
+        console.log(
+          chalk.green.bold(`\n 🎉 A new file was create at: ${filePath} \n`)
+        );
+      }
+    });
   });
 
 program.parse(process.argv);
